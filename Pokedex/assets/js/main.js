cc18ef8ffd30539c5b550e15762238e93cdc7682
Jacqueline Atae <dev.atae02@gmail.com>
2023-04-04 21:46:44 -0300

    Dominando o Protocolo HTTP e Integrando com a PokeAPI

---
+++ Pokedex/assets/js/main.js
@@ -0,0 +1,45 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButtom = document.getElementById('loadMoreButtom')
+
+const maxRecords = 151
+const limit = 5
+let offset = 0
+
+
+function loadPokemonItens(offset, limit) {
+     //uso de arrow function - funcao de callback
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            
+            <div class="detail">
+                <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}                
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        </li>
+    `).join('')
+
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset,limit)
+
+loadMoreButtom.addEventListener('click', () => {
+    offset += limit
+
+    const qtdRecordNextPage = offset + limit
+
+    if(qtdRecordNextPage >= maxRecords){
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButtom.parentElement.removeChild(loadMoreButtom)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
